58a3068667dc5f89e08fcb6c2ceae7af7f0d0a77
Jose1-0 <josealedrr@gmail.com>
2024-11-12 18:04:31 -0300

    adicionando emulador

--- AppGraficos/src/app/chart-barras/chart-barras.module.ts
+++ AppGraficos/src/app/chart-barras/chart-barras.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ChartBarrasPageRoutingModule } from './chart-barras-routing.module';
-
 import { ChartBarrasPage } from './chart-barras.page';
 
 @NgModule({
