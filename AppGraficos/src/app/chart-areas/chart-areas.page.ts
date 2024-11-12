8e9a30540b4f48e6b74697c257f41a4db63f7f24
JpMeira <jp.mpmeira@gmail.com>
2024-11-12 19:52:30 -0300

    Subindo gráfico áreas

--- AppGraficos/src/app/chart-areas/chart-areas.page.ts
+++ AppGraficos/src/app/chart-areas/chart-areas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart-areas',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart-areas.page.scss'],
 })
 export class ChartAreasPage implements OnInit {
-
-  constructor() { }
+  chart: any;
 
   ngOnInit() {
+    
+    this.createChart();
   }
 
+  createChart() {
+    const ctx = document.getElementById('canvas-areas') as HTMLCanvasElement;
+    this.chart = new Chart(ctx, {
+      type: 'line',  // Usando tipo 'line' para área
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [{
+          label: 'Vendas',
+          data: [65, 59, 80, 81, 56, 55],
+          fill: true,  // Preenchendo a área abaixo da linha
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
